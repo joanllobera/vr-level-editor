2be5bd691f8635d30031585b61e4f9753a421c1e
Albert Vegara <albertvegaramartinez@enti.cat>
2020-03-23 14:23:35 +0100

    Added Oculus VR and a cube to the hand

---
+++ Level-Creator-VR/Assets/Scripts/Cube_Hand_Behaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube_Hand_Behaviour : MonoBehaviour
+{
+    [SerializeField] Transform handParent;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.Round(handParent.position.x),
+                                         Mathf.Round(handParent.position.y),
+                                         Mathf.Round(handParent.position.z));
+
+        //transform.rotation = Quaternion.identity;
+    }
+}
