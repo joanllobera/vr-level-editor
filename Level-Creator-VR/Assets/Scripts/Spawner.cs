48cd6aeb5574bbddf344e52b12e40e3482ee62ff
Alex Gomez Cortes <gomezcortes10@gmail.com>
2020-03-26 16:02:27 +0100

    object spawner

---
+++ Level-Creator-VR/Assets/Scripts/Spawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    private bool spawnOnce;
+    public List<GameObject> spawnedObjects;
+    public GameObject cube;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+        if (Input.GetAxis("Oculus_CrossPlatform_SecondaryIndexTrigger") > 0.3f && !spawnOnce)
+        {
+            spawnOnce = true;
+            spawnedObjects.Add(Instantiate(cube, cube.transform.position, cube.transform.rotation));
+            spawnedObjects[spawnedObjects.Count - 1].GetComponent<Cube_Hand_Behaviour>().enabled = false; //desactivo el script para que no siga a la mano
+
+        }
+        else
+        {
+            if (Input.GetAxis("Oculus_CrossPlatform_SecondaryIndexTrigger") < 0.3f)
+                spawnOnce = false;
+        }
+           
+    }
+}
