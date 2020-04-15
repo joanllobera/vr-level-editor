cc703ce0f6d3ce2cbc5ee98fd0cf5a0a1f27b0e9
Albert Vegara <albertvegaramartinez@enti.cat>
2020-04-15 19:05:29 +0200

    Working on snapping things

--- Level-Creator-VR/Assets/Scripts/Player_Hand_Behaviour.cs
+++ Level-Creator-VR/Assets/Scripts/Player_Hand_Behaviour.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class Player_Hand_Behaviour : MonoBehaviour
 {
+    enum Direction {Up, Down, Right, Left, Forward, Back};
+    struct RaycastedObject
+    {
+        public Transform transform;
+        public Direction direction;
+
+        public RaycastedObject(Transform _transform, Direction _direction)
+        {
+            transform = _transform;
+            direction = _direction;
+        }
+    }
     [SerializeField] Transform handParent;
-    private List<Transform> raycastedObjects;
+    private List<RaycastedObject> raycastedObjects;
 
     //hit for distance checking
     RaycastHit hit;
@@ -13,66 +25,125 @@ public class Player_Hand_Behaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = handParent.transform.position;//new Vector3(Mathf.Round(handParent.position.x),
+        //new Vector3(Mathf.Round(handParent.position.x),
                                          //Mathf.Round(handParent.position.y),
                                          //Mathf.Round(handParent.position.z));
 
         //transform.rotation = Quaternion.identity;
 
-        raycastedObjects = new List<Transform>();
+        raycastedObjects = new List<RaycastedObject>();
 
         //Raycast up
         if(Physics.Raycast(transform.position, Vector3.up, out hit, 2f))
         {
-            if(hit.transform.tag == "Floor")
+            if(hit.transform.tag == "cube")
             {
-                raycastedObjects.Add(hit.transform);
+                RaycastedObject obj = new RaycastedObject(hit.transform, Direction.Up);
+                raycastedObjects.Add(obj);
             }
         }
         //Raycast down
         if(Physics.Raycast(transform.position, Vector3.down, out hit, 2f))
         {
-            if(hit.transform.tag == "Floor")
+            if(hit.transform.tag == "cube")
             {
-                raycastedObjects.Add(hit.transform);
+                RaycastedObject obj = new RaycastedObject(hit.transform, Direction.Down);
+                raycastedObjects.Add(obj);
             }
         }   
         //Raycast Right
         if(Physics.Raycast(transform.position, Vector3.right, out hit, 2f))
         {
-            if(hit.transform.tag == "Floor")
+            if(hit.transform.tag == "cube")
             {
-                raycastedObjects.Add(hit.transform);
+                RaycastedObject obj = new RaycastedObject(hit.transform, Direction.Right);
+                raycastedObjects.Add(obj);
             }
         }  
         //Raycast Left
         if(Physics.Raycast(transform.position, Vector3.left, out hit, 2f))
         {
-            if(hit.transform.tag == "Floor")
+            if(hit.transform.tag == "cube")
             {
-                raycastedObjects.Add(hit.transform);
+                RaycastedObject obj = new RaycastedObject(hit.transform, Direction.Left);
+                raycastedObjects.Add(obj);
             }
         }  
         //Raycast forward
         if(Physics.Raycast(transform.position, Vector3.forward, out hit, 2f))
         {
-            if(hit.transform.tag == "Floor")
+            if(hit.transform.tag == "cube")
             {
-                raycastedObjects.Add(hit.transform);
+                RaycastedObject obj = new RaycastedObject(hit.transform, Direction.Forward);
+                raycastedObjects.Add(obj);
             }
         }  
         //Raycast backwards
         if(Physics.Raycast(transform.position, Vector3.back, out hit, 2f))
         {
-            if(hit.transform.tag == "Floor")
+            if(hit.transform.tag == "cube")
             {
-                raycastedObjects.Add(hit.transform);
+                RaycastedObject obj = new RaycastedObject(hit.transform, Direction.Back);
+                raycastedObjects.Add(obj);
             }
         }  
 
-        foreach(Transform t in raycastedObjects)
-        {
+        float minDist = Mathf.Infinity;
+        RaycastedObject closestObject = new RaycastedObject(null, Direction.Up);
 
+        foreach(RaycastedObject obj in raycastedObjects)
+        {
+            float dist = Vector3.Distance(obj.transform.position, transform.position);
+            if(dist < minDist)
+            {
+                closestObject = obj;
+                minDist = dist;
+            }
+        }
+
+        if(raycastedObjects.Count > 0)
+        {
+            float x = 0;
+            float y = 0;
+            float z = 0;
+            switch(closestObject.direction)
+            {
+                case Direction.Up:
+                    x = closestObject.transform.position.x;
+                    y = closestObject.transform.position.y - closestObject.transform.localScale.y;
+                    z = closestObject.transform.position.z;
+                    break;
+                case Direction.Down:
+                    x = closestObject.transform.position.x;
+                    y = closestObject.transform.position.y + closestObject.transform.localScale.y;
+                    z = closestObject.transform.position.z;
+                    break;
+                case Direction.Left:
+                    x = closestObject.transform.position.x + closestObject.transform.localScale.x;
+                    y = closestObject.transform.position.y;
+                    z = closestObject.transform.position.z;
+                    break;
+                case Direction.Right:
+                    x = closestObject.transform.position.x - closestObject.transform.localScale.x;
+                    y = closestObject.transform.position.y;
+                    z = closestObject.transform.position.z;
+                    break;
+                case Direction.Forward:
+                    x = closestObject.transform.position.x;
+                    y = closestObject.transform.position.y;
+                    z = closestObject.transform.position.z - closestObject.transform.localScale.z;
+                    break;
+                case Direction.Back:
+                    x = closestObject.transform.position.x;
+                    y = closestObject.transform.position.y;
+                    z = closestObject.transform.position.z + closestObject.transform.localScale.z;
+                    break;
+            }
+            transform.position = new Vector3(x, y, z);
+        }
+        else
+        {
+            transform.position = handParent.transform.position;
         }
 
     }
