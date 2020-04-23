7babae3117d4c0d4c3864f084784215e4d94353b
Adam <adamtubauvilar@enti.cat>
2020-04-23 13:31:53 +0200

    ready Play

---
+++ Level-Creator-VR/Assets/Scripts/canStart.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class canStart : MonoBehaviour
+{
+    float timer = 3f;
+
+    void startButton()
+    {
+        if (GameObject.Find("Player") != null && GameObject.Find("Goal") != null && GameObject.Find("Light") != null)
+        {
+            //play function
+        }
+        else
+        {
+            timer = 3f;
+            GameObject.Find("Text").GetComponent<Text>().text = "Something missing";
+        }
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer<=0)
+        {
+            GameObject.Find("Text").GetComponent<Text>().text = "";
+        }
+
+        //////provisional
+        if(Input.GetKeyDown(KeyCode.V))
+        {
+            startButton();
+        }
+    }
+}
