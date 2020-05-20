6fe7ae2808bf732496d7387e8c345294ad212a60
Adam <adamtubauvilar@enti.cat>
2020-05-21 00:16:53 +0200

    buttonFeedback

---
+++ Level-Creator-VR/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource UIAudio;
+    [SerializeField]
+    AudioSource UndoAudio;
+    [SerializeField]
+    AudioSource RedoAudio;
+    [SerializeField]
+    AudioSource PlaceAudio;
+    [SerializeField]
+    AudioSource EraseAudio;
+    [SerializeField]
+    AudioSource SaveAudio;
+
+    void ButtonUI()
+    {
+        UIAudio.Play();
+    }
+    void Undo()
+    {
+        UndoAudio.Play();
+    }
+    void Redo()
+    {
+        RedoAudio.Play();
+    }
+    void Place()
+    {
+        PlaceAudio.Play();
+    }
+    void Erase()
+    {
+        EraseAudio.Play();
+    }
+    void Save()
+    {
+        SaveAudio.Play();
+    }
+}
