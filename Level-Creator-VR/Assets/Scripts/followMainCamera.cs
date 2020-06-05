fbf46a6b3478e9bd2339162e4ce73207de7baa0a
Adam <adamtubauvilar@enti.cat>
2020-06-06 00:45:02 +0200

    controls panel

---
+++ Level-Creator-VR/Assets/Scripts/followMainCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followMainCamera : MonoBehaviour
+{
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         player = GameObject.Find("Player");        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + transform.forward * 15 + new Vector3(2,-2,0);
+    }
+}
