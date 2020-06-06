40175fb5d525bcb48b835da89e57e2dac0073770
David Sola <davidcansell5@gmail.com>
2020-06-06 17:01:10 +0200

    Updated main menu layout & button behaviour script

---
+++ Level-Creator-VR/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonController : MonoBehaviour
+{
+    private bool isPressed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPressed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void changeColor(string tag)
+    {
+        if(tag == gameObject.tag)
+        {
+            isPressed = true;
+        }
+        else{
+            isPressed = false;
+        }
+
+        if(!isPressed){
+            GetComponentInChildren<Image>().color = Color.blue;
+        }
+        else if(isPressed){
+            GetComponentInChildren<Image>().color = Color.yellow;
+        }
+
+        
+    }
+}
